5d0b93d5165ea1612c3c854d672cc2693852d331
JusceCabrera <jusce240@gmail.com>
2024-07-17 23:05:40 -0500

    add products test

--- desafios_entregables/desafio_entregable9/src/routes/products.router.js
+++ desafios_entregables/desafio_entregable9/src/routes/products.router.js
@@ -11,6 +11,6 @@ router.get("/api/products", getProducts);
 router.get("/api/products/:pid", getProductByID);
 router.post("/api/products", addProduct);
 router.put("/api/products", updateProduct);
-router.delete("/api/products", deleteProduct);
+router.delete("/api/products/:pid", deleteProduct);
 
 export default router;
